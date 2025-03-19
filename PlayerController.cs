f522a3979d9cd302b507c04db308b2298802bd47
BLAZE <basohblaise23@gmail.com>
2025-03-20 00:42:43 +0100

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,25 +1,45 @@
+using System;
 using UnityEngine;
+using static Container;
 
 public class PlayerController : MonoBehaviour
 {
-    public Vector2 moveInput;
-    public Vector2 viewInput;
+    private Vector2 moveInput;
+    private Vector2 viewInput;
+    public PlayerSettings playerSettings;
+    [HideInInspector] public CharacterController characterController;
+    public Transform cameraTransform;
+    private Vector3 newCameraRotation;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        CalculateMovement();
+    }
 
     void Update()
     {
+        CalculateView();
+    }
+
+    void CalculateMovement()
+    {
         moveInput = InputManager.Instance.MoveAction.ReadValue<Vector2>();
+
+    }
+
+    void CalculateView()
+    {
         viewInput = InputManager.Instance.ViewAction.ReadValue<Vector2>();
 
-        // Check if jump is triggered
-        if (InputManager.Instance.JumpAction.WasPressedThisFrame())
-        {
-            Debug.Log("Jump Pressed!");
-        }
+        newCameraRotation.x += viewInput.y * playerSettings.ViewYSensitivity * (playerSettings.ViewYInverted ? 1 : -1) * Time.deltaTime;
 
-        // Check if sprint is held
-        if (InputManager.Instance.SprintAction.IsPressed())
-        {
-            Debug.Log("Sprinting...");
-        }
+        newCameraRotation.x = Mathf.Clamp(newCameraRotation.x, playerSettings.ViewMinClamp, playerSettings.ViewMaxClamp);
+
+        cameraTransform.localRotation = Quaternion.Euler(newCameraRotation);
     }
 }
