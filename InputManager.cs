908068aee999bf49f16d3c7659765e5f756c943b
BLAZE <basohblaise23@gmail.com>
2025-03-23 19:29:03 +0100

    Update InputManager.cs

--- InputManager.cs
+++ InputManager.cs
@@ -4,30 +4,32 @@ using UnityEngine.InputSystem;
 public class InputManager : MonoBehaviour
 {
     public static InputManager Instance { get; private set; }
-
     private InputSystem_Actions inputSystem;
-
     public InputAction MoveAction { get; private set; }
     public InputAction ViewAction { get; private set; }
     public InputAction JumpAction { get; private set; }
+    public InputAction CrouchAction { get; private set; }
     public InputAction SprintAction { get; private set; }
 
     private void Awake()
     {
+
         if (Instance == null)
+        {
             Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
         else
         {
             Destroy(gameObject);
-            return;
         }
-
         inputSystem = new InputSystem_Actions();
 
-        // Assign actions
+        // Player actions
         MoveAction = inputSystem.Player.Move;
         ViewAction = inputSystem.Player.View;
         JumpAction = inputSystem.Player.Jump;
+        CrouchAction = inputSystem.Player.Crouch;
         SprintAction = inputSystem.Player.Sprint;
 
         inputSystem.Enable();
